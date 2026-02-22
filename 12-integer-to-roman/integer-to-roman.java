9dc1d2e7eca475267e7b470ca42845cf58f07a9c
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-02-23 03:59:01 +0530

    Time: 3 ms (99.60%) | Memory: 46.7 MB (26.43%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public String intToRoman(int num) {
+        int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symb = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<13;i++){
+            if(num == 0) break;
+
+            int times = num / val[i];
+            while(times>0){
+                sb.append(symb[i]);
+                times--;
+            }
+            
+            num = num % val[i];
+        }
+
+        return sb.toString();
+    }
+}
