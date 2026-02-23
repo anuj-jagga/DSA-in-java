be022532a91109901e96935487d5d8e34dd065f3
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-02-23 05:58:26 +0530

    Time: 6 ms (99.37%) | Memory: 49.7 MB (75.18%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,43 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
-        boolean[] vis = new boolean[n];
-        List<List<String>> res = new ArrayList<>();
-        int idx = 0;
-        for(int i=0;i<n;i++){
-            if(vis[i]) continue;
-            String s = strs[i];
-            res.add(new ArrayList<>());
-            res.get(idx).add(s);
-            for(int j = i+1;j<n;j++){
-                String t = strs[j];
-                if(isAnagram(s,t)){
-                    res.get(idx).add(t);
-                    vis[j] = true;
-                }
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            idx++;
-        }
-
-        return res;
-    }
-
-     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-
-        int[] count = new int[26];
-
-        for(char c:s.toCharArray()){
-            count[c-'a']++;
-        }
-
-        for(char c:t.toCharArray()){
-            count[c-'a']--;
-            if(count[c-'a']<0) return false;
+            
+            map.get(sortedWord).add(word);
         }
         
-        return true;
+        return new ArrayList<>(map.values());   
     }
 }
