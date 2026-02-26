733ea2419aa4be205eb9c3a4a79dc8f036b435d8
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-02-26 23:45:33 +0530

    Time: 2 ms (99.11%) | Memory: 119.7 MB (24.22%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -4,22 +4,24 @@ class Solution {
         for(int c:cost) costSum+=c;
         int gasSum = 0;
         for(int g:gas) gasSum+=g;
-        if(costSum > gasSum) return -1;
+        if(costSum > gasSum) return -1;  //completing the circle is impossible from any index.
 
         int sum = 0;
         int idx = 0;
 
         for(int i=0;i<gas.length;i++){
-            sum += gas[i];
-            sum -= cost[i];
+            // sum += gas[i];
+            // sum -= cost[i];
 
             if(sum < 0){
                 sum = 0;
-                idx = i+1;
+                idx = i;
             }
+            sum += gas[i];
+            sum -= cost[i];
+
         }
 
-        // return idx<gas.length? idx : -1;
         return idx;
     }
 }
