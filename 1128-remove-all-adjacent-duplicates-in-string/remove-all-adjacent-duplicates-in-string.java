d3d1210fb152341b9071c3d1b8c24df7ca01082a
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-03-10 12:45:43 +0530

    Time: 14 ms (73.56%) | Memory: 46.3 MB (96.27%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,14 +1,12 @@
 class Solution {
     public String removeDuplicates(String s) {
-        int len = 0;
         StringBuilder sb = new StringBuilder();
+
         for(int i=0;i<s.length();i++){
-            if(len!=0 && s.charAt(i) == sb.charAt(len-1)){
+            if(sb.length()!=0 && s.charAt(i) == sb.charAt(sb.length()-1)){
                 sb.deleteCharAt(sb.length()-1);
-                len--;
             }else{
                 sb.append(s.charAt(i));
-                len++;
             }
         }
         return sb.toString();
