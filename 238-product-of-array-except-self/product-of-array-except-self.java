2a33f11270b4783d73ddbf776d27557e9ac138b1
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-03-11 18:39:51 +0530

    Time: 2 ms (94.55%) | Memory: 64.1 MB (94.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -6,17 +6,12 @@ class Solution {
         for(int i=1;i<n;i++){
             prefix[i] = prefix[i-1] * nums[i-1];
         }
-
-        int[] suffix = new int[n];
-        suffix[n-1] = 1;
-
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = suffix[i+1] * nums[i+1];
-        }
-
+   
         int[] res = new int[n];
-        for(int i=0;i<n;i++){
-            res[i] = suffix[i] * prefix[i];
+        int suffix = 1;
+        for(int i=n-1;i>=0;i--){
+            res[i] = prefix[i] * suffix;
+            suffix *= nums[i];
         }
         return res;
     }
