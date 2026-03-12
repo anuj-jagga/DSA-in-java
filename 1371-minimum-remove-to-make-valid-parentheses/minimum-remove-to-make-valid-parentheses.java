706b6aa42a6527eb9f13ff39573da0a0ea74755e
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-03-12 09:58:28 +0530

    Time: 580 ms (5.00%) | Memory: 47.4 MB (49.49%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,36 +1,29 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        ArrayList<Integer> list1 = new ArrayList<>();
-        Stack<int[]> st = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
         for(int i=0; i<s.length();i++){
             char ch = s.charAt(i);
             if(ch == '('){
-                st.add(new int[]{ch,i});
+                st.push(i);
             }
             else if(ch == ')'){
                 if(st.isEmpty()){
-                    list1.add(i);
+                    list.add(i);
                 }else{
                     st.pop();
                 }
             }
         }
 
+        while(!st.isEmpty()){
+            list.add(st.pop());
+        }
         StringBuilder res = new StringBuilder();
-         ArrayList<Integer> list2 =  new ArrayList<>();
 
-        if(!st.isEmpty()){
-            for(int[] a : st){
-                list2.add(a[1]);
-            }
+        for(int i=0; i< s.length();i++){
+            if(!list.contains(i)) res.append(s.charAt(i));
         }
-
-        for(int i=0;i<s.length();i++){
-            if(list2.contains(i) || list1.contains(i)) continue;
-            res.append(s.charAt(i));
-        }
-        
-
         return res.toString();
     }
 }
