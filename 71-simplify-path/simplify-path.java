a53928bc3583db7800c30e8544d3f4544e0937d6
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-03-12 11:40:34 +0530

    Time: 4 ms (95.85%) | Memory: 44.1 MB (99.50%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,42 +1,23 @@
 class Solution {
-    public String simplifyPath(String s) {
-        ArrayList<String> list = new ArrayList<>();
-        int n = s.length();
-        String curr = "";
-
-        for(int i = 1; i < n; i++){
-            char ch = s.charAt(i);
-
-            if(ch == '/' && s.charAt(i-1) != '/'){
-                list.add(curr);
-                curr = "";
-                continue;
-            }
-
-            if(ch != '/')
-                curr += ch;
-        }
-
-        list.add(curr);
-
-        ArrayList<String> stack = new ArrayList<>();
-
-        for(String str : list){
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        for(String str : path.split("/")){
             if(str.equals("") || str.equals(".")){
                 continue;
             }
             else if(str.equals("..")){
-                if(stack.size() > 0)
-                    stack.remove(stack.size() - 1);
+                if(st.size() > 0){
+                    st.pop();
+                }
             }
             else{
-                stack.add(str);
+                st.push(str);
             }
         }
 
         StringBuilder res = new StringBuilder();
 
-        for(String dir : stack){
+        for(String dir : st){
             res.append("/").append(dir);
         }
 
