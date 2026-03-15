e0996937f8116f68b1f8b2269c4c82cc7b5b92dc
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-03-15 12:55:56 +0530

    Time: 36 ms (25.67%) | Memory: 59.3 MB (16.46%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,6 +1,12 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> maxHeap =  new PriorityQueue<> ((a,b)-> (int)((Math.pow(b[0],2) + Math.pow(b[1],2)) - (Math.pow(a[0],2) + Math.pow(a[1],2))));
+        // PriorityQueue<int[]> maxHeap =  new PriorityQueue<> ((a,b)-> (int)((Math.pow(b[0],2) + Math.pow(b[1],2)) - (Math.pow(a[0],2) + Math.pow(a[1],2))));
+
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> {
+            int distA = a[0] * a[0] + a[1] * a[1];
+            int distB = b[0] * b[0] + b[1] * b[1];
+            return Integer.compare(distB, distA);
+        });
 
         for(int[] a : points){
             maxHeap.add(new int[]{a[0],a[1]}); 
