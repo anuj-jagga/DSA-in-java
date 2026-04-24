c5574b1b4950bcd9425d33c6461edb2bd1698c87
anuj-jagga <157093958+anuj-jagga@users.noreply.github.com>
2026-04-25 00:58:52 +0530

    Time: 274 ms (44.02%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,5 @@
-SELECT MAX(salary) AS secondHighestSalary
+SELECT MAX(salary) AS SecondHighestSalary
 FROM Employee
 WHERE salary < (
-    SELECT MAX(salary)
-    FROM Employee
+    Select MAX(salary) FROM Employee
 )
